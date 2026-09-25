7fb877970a418eb513a555a347f2b4125c57b37b
Ramon Williams <24hrautollc@gmail.com>
2026-09-24 20:00:50 -0400

    Load mailbox row layout fix

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import "./styles/globals.css";
 import "./styles/auth.css";
 import "./styles/mail.css";
+import "./styles/mail-row-fix.css";
 import "./styles/product.css";
 import "./styles/calendar.css";
 import { AuthGate } from "./AuthGate";
